990a9eb1e25a22a7acea90c7c4e26192ecd90810
Bravowhale <Bravowhale@Bravowhale-PC>
2016-08-08 18:21:56 +0800

    Jedis,以及HelloWorld字节码分析

---
+++ Test/src/com/cache/test/Test111.java
@@ -0,0 +1,12 @@
+package com.cache.test;
+
+import java.util.Random;
+
+public class Test111 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(new Random().nextInt(10));
+	}
+
+}
