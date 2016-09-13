d88edcff56b07f75d5664b380479fb2946b9795f
huangjingcheng <Bravowhale@Bravowhale-PC>
2016-09-13 18:18:57 +0800

    Add Numbers

---
+++ Test/src/Solution.java
@@ -0,0 +1,32 @@
+
+public class Solution {
+	public ListNode addTwoNumbers(ListNode l1,ListNode l2){
+		if(l1 == null) return l2;
+		if(l2 == null) return l1;
+		ListNode lastList = new ListNode(0);
+		ListNode ret = new ListNode(0);
+		while(l1 != null && l2 != null){
+			ret.val = (l1.val + l2.val)%10;
+			ret.next = new ListNode(0);
+			l1 = l1.next;
+			l2 = l2.next;
+		}
+		if(l1 != null){//l1还有的话，加在其后即可。
+			ret.next = l1;
+		}
+		if(l2 != null){
+			ret.next = l2;
+		}
+		return ret;
+	}
+	public static void main(String[] args){
+		ListNode l1 = new ListNode(2);
+		l1.next = new ListNode(4);
+		l1.next.next = new ListNode(3);
+		ListNode l2 = new ListNode(5);
+		l2.next = new ListNode(6);
+		l2.next.next = new ListNode(4);
+		ListNode listNode = new Solution().addTwoNumbers(l1, l2);
+		System.out.println(listNode.next.next.val);
+	}
+}
--- Test/src/Test.java
+++ Test/src/Test.java
@@ -1,4 +1,5 @@
 import java.lang.reflect.Field;
+import java.util.Hashtable;
 
 public class Test implements Cloneable{
 	public static void main(String[] args) throws Exception{
@@ -9,5 +10,36 @@ public class Test implements Cloneable{
 		System.out.println(hello);
 		System.out.println(value.get(hello));
 		System.out.println(value.get(hello_world));
+		System.setSecurityManager(new SecurityManager(){
+			@Override
+			public void checkExit(int status) {
+				// TODO Auto-generated method stub
+				throw new ThreadDeath();
+			}
+		});
+		Hashtable<Integer,Integer> hash = new Hashtable<Integer,Integer>();
+		/*try{
+			System.exit(0);
+		}finally{
+			System.out.println("In the finnaly block");
+		}*/
+		System.out.println(Singleton1.getSingleton().count1);
+		System.out.println(Singleton1.getSingleton().count2);
+	}
+	
+}
+
+class Singleton1{
+	public static int count2 = 0;
+	public static int count1 = 2;
+//	private static Singleton1 singleton1= new Singleton1();
+	
+	
+	private Singleton1(){
+		count1 = count1 +1 ;
+		count2++;
+	}
+	public static Singleton1 getSingleton(){
+		return new Singleton1();
 	}
 }
