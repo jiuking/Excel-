c1f2a0dae1a92889c28583fdfd5e5d358f989f58
huangjingcheng <Bravowhale@Bravowhale-PC>
2016-09-20 18:25:53 +0800

    leetcode 226

---
+++ Test/src/com/leetcode/solution/Question226.java
@@ -0,0 +1,32 @@
+package com.leetcode.solution;
+
+//Invert Binary Tree
+
+/*Invert a binary tree.
+
+    4
+  /   \
+ 2     7
+/ \   / \
+1   3 6   9
+to
+    4
+  /   \
+ 7     2
+/ \   / \
+9   6 3   1*/
+public class Question226 {
+	public TreeNode226 invertTree(TreeNode226 root) {
+		return null;
+	}
+}
+
+class TreeNode226{
+	int val;
+	TreeNode226 left;
+	TreeNode226 right;
+
+	TreeNode226(int x) {
+		val = x;
+	}
+}
